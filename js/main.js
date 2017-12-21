068a2e33fef37dd5920fc00703a21c23b910574d
Eric Marrone <emarrone17@gmail.com>
2017-12-21 16:05:35 -0500

    Complete exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var body = document.body
+  var statusHeader = document.querySelector('.status');
+  var lightSwitch = document.querySelector('.switch');
 
+  // statusHeader.innerText = "Ready";
 
+  lightSwitch.addEventListener('click', function(e) {
+    if (e.target.classList.contains('on')) {
+      e.target.classList.remove('on');
+      e.target.classList.add('off');
+      body.classList.remove('light');
+      body.classList.add('dark');
+      statusHeader.innerText = "Hey, who turned off the lights?";
+    }else if (e.target.classList.contains('off')) {
+      e.target.classList.remove('off');
+      e.target.classList.add('on');
+      body.classList.remove('dark');
+      body.classList.add('light');
+      statusHeader.innerText = "It's so bright in here!";
+    }
+  })
+});
